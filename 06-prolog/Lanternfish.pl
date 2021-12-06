f1bff990dd84403dbb4d315d7d9a34593af0faec
kmolski <starfighter413@gmail.com>
2021-12-06 14:19:32 +0100

    Add solution for day 6

--- 06-prolog/Lanternfish.pl
+++ 06-prolog/Lanternfish.pl
@@ -1,35 +1,37 @@
-:- use_module(library(clpfd)).
-:- use_module(library(lists)).
 :- initialization(main).
+
+:- table new_fish/3.
+:- table count_fish/3.
 
 string_number(Str, Num) :- number_string(Num, Str).
 
 stream_fields(In, Fields) :-
     read_string(In, _, Str),
-    split_string(Str, ",", "\n", L),
-    maplist(string_number, L, Fields).
+    split_string(Str, ",", "\n", Fs),
+    maplist(string_number, Fs, Fields).
 
 file_fields(Filename, Fields) :-
     setup_call_cleanup(open(Filename, read, In),
         stream_fields(In, Fields),
         close(In)).
 
-% S - simulation start time,            E - simulation end time
-% C - days until new lanternfish spawn, D - number of descendants
-% N prefix - new value
-count_fish_and_descendants(0, _, 1).
-count_fish_and_descendants(E, 0, D) :-
-    NE is E - 1,
-    count_fish_and_descendants(NE, 8, CR), % count descendants for child
-    count_fish_and_descendants(NE, 6, TD), % count descendants for this
-    D is TD + CR.
+% E - days until simulation end, S - amount of spawns left
+% D - number of descendants, N prefix - updated value
+new_fish(E, I, D) :- NE is E - I * 7, count_fish(NE, 8, D).
 
-count_fish_and_descendants(E, C, D) :-
-    NE is E - 1, NC is C - 1, count_fish_and_descendants(NE, NC, D).
+count_fish(E, _, 1) :- E =< 0. % a fish can't spawn if there are zero days until end
+count_fish(E, 0, D) :-
+    NE is E - 1, S is div(NE, 7),
+    numlist(0, S, I),
+    maplist(new_fish(NE), I, Counts),
+    sum_list(Counts, CSum),
+    D is 1 + CSum.
+
+% C - days until new spawn
+count_fish(E, C, D) :- NE is E - C, count_fish(NE, 0, D).
 
 solve(Fields, E, Result) :-
-    maplist(count_fish_and_descendants(E), Fields, Counts),
-    sum_list(Counts, Result).
+    maplist(count_fish(E), Fields, Counts), sum_list(Counts, Result).
 
 main :-
     current_prolog_flag(argv, [Filename | _]),
---
+++ 06-prolog/README.md
@@ -0,0 +1,21 @@
+# [Day 6: Lanternfish](https://adventofcode.com/2021/day/6) solution
+
+## Expected result
+```
+$ make run
+swipl Lanternfish.pl example.txt
+Part 1: 5934
+Part 2: 26984457539
+swipl Lanternfish.pl data.txt
+Part 1: 362666
+Part 2: 1640526601595
+$ make test
+swipl Lanternfish.pl example.txt | diff - example.out
+swipl Lanternfish.pl data.txt | diff - data.out
+```
+
+## Tested on
+```
+$ swipl --version
+SWI-Prolog version 8.2.3 for amd64-freebsd
+```
