0a741384bcc839dd929657acd9cc1a54c5c759a6
kmolski <starfighter413@gmail.com>
2021-12-09 20:25:29 +0100

    Add solution for day 9 part 2

---
+++ 09-pascal/README.md
@@ -0,0 +1,24 @@
+# [Day 9: Smoke Basin](https://adventofcode.com/2021/day/9) solution
+
+## Expected result
+```
+$ make run
+./SmokeBasin example.txt; ./SmokeBasin data.txt
+Part 1: 15
+Part 2: 1134
+Part 1: 537
+Part 2: 1142757
+$ make test
+./SmokeBasin example.txt | diff - example.out
+./SmokeBasin data.txt | diff - data.out
+```
+
+## Tested on
+```
+$ fpc -i
+Free Pascal Compiler version 3.2.2
+
+Compiler date      : 2021/10/05
+Compiler CPU target: x86_64
+...
+```
--- 09-pascal/SmokeBasin.pas
+++ 09-pascal/SmokeBasin.pas
@@ -1,19 +1,143 @@
 program SmokeBasin;
 
-type Point = record
-         x, y: Integer;
-     end;
-
-// function findLowPoints(heatmap: Array of Array of Integer): Array of Point;
-// begin
-// end;
+type Heatmap = Array of Array of Integer;
+type Point = record x, y: Integer end;
 
 var inputFile: Text;
-    rowCount, rowLength, riskLevelSum, i, j, m, n: Integer;
+    rowCount, rowLength, i: Integer;
+    part1, part2: LongInt;
     line: String;
+    hmap: Heatmap;
+    points: Array of Point;
+
+procedure FindLowPoints(hmap: Heatmap);
+var p: Point;
     isLowPoint: Boolean;
-    heatmap: Array of Array of Integer;
-    row: Array of Integer; // REMOVE
+    i, j, m, n: Integer;
+    pointCount: Integer = 0;
+begin
+    SetLength(points, length(hmap) * length(hmap[0])); // max possible size
+    for i := low(hmap) to high(hmap) do
+    begin
+        for j := low(hmap[i]) to high(hmap[i]) do
+        begin
+            isLowPoint := true;
+            for m := -1 to 1 do // low point if all neighbours are higher
+            begin
+                for n := -1 to 1 do
+                begin
+                    if not ((m = 0) and (n = 0)) and
+                       (low(hmap) <= i + m) and (i + m <= high(hmap)) and
+                       (low(hmap[i]) <= j + n) and (j + n <= high(hmap[i])) then
+                        isLowPoint := isLowPoint and (hmap[i, j] < hmap[i + m, j + n]);
+                end;
+            end;
+
+            if isLowPoint then // append low point
+            begin
+                p.y := i; p.x := j;
+                points[pointCount] := p;
+                Inc(pointCount);
+            end;
+        end;
+    end;
+    SetLength(points, pointCount); // shrink array to fit
+end;
+
+function solvePart1(): Integer;
+var p: Point;
+begin
+    solvePart1 := 0;
+    for p in points do
+    begin
+        solvePart1 += (1 + hmap[p.y, p.x]);
+    end;
+end;
+
+function pointExists(positions: Array of Point; np: Point): Boolean;
+var p: Point;
+begin
+    for p in positions do
+    begin
+        if (p.x = np.x) and (p.y = np.y) then
+            Exit(true);
+    end;
+    pointExists := false;
+end;
+
+function validPointOffset(p: Point; i, j: Integer): Boolean;
+begin
+    // check if point is in bounds, and its value on the heatmap
+    // is smaller than 9 and larger than the previous point
+    validPointOffset := (low(hmap) <= p.y + i) and (p.y + i <= high(hmap)) and
+        (low(hmap[p.y]) <= p.x + j) and (p.x + j <= high(hmap[p.y])) and
+        (hmap[p.y + i, p.x + j] < 9) and (hmap[p.y + i, p.x + j] > hmap[p.y, p.x]);
+end;
+
+function computeBasinSize(p: Point): Integer;
+var temp: Point;
+    i, j, k, positionCount, start, newStart: Integer;
+    positions: Array of Point;
+    gotNewPositions: Boolean = true;
+    offset: Array[1..2] of Integer;
+    offsets: Array[1..4, 1..2] of Integer = ((1, 0), (-1, 0), (0, 1), (0, -1));
+begin
+    positionCount := 1; start := 0;
+    SetLength(positions, positionCount);
+    positions[0] := p;
+
+    while gotNewPositions do
+    begin
+        gotNewPositions := false;
+        newStart := high(positions) + 1;
+        for k := start to high(positions) do // start off with newly added positions
+        begin
+            for offset in offsets do
+            begin
+                i := offset[1]; j := offset[2];
+                if validPointOffset(positions[k], i, j) then
+                begin
+                    temp.y := positions[k].y + i; temp.x := positions[k].x + j;
+                    if not pointExists(positions, temp) then // add if point does not exist
+                    begin
+                        gotNewPositions := true;
+                        SetLength(positions, positionCount + 1);
+                        positions[positionCount] := temp;
+                        Inc(positionCount);
+                    end;
+                end;
+            end;
+        end;
+        start := newStart; // set new start position
+    end;
+    computeBasinSize := positionCount;
+end;
+
+function solvePart2(): LongInt;
+var i, j, index, len: Integer;
+    basinSizes: Array of Integer;
+begin
+    len := length(points);
+
+    SetLength(basinSizes, len);
+    for i := 0 to len do
+    begin
+        basinSizes[i] := computeBasinSize(points[i]);
+    end;
+
+    for i := 2 to len - 1 do // insertion sort
+    begin
+        index := basinSizes[i];
+        j := i;
+        while ((j > 1) and (basinSizes[j - 1] > index)) do
+        begin
+            basinSizes[j] := basinSizes[j-1];
+            j := j - 1;
+        end;
+        basinSizes[j] := index;
+    end;
+    solvePart2 := basinSizes[len - 1] * basinSizes[len - 2] * basinSizes[len - 3];
+end;
 
 BEGIN
 
@@ -27,39 +151,22 @@ begin
     begin
         Readln(inputFile, line);
         rowLength := length(line);
-        SetLength(heatmap, rowCount + 1, rowLength);
-        for i := 1 to rowLength do
+        SetLength(hmap, rowCount + 1, rowLength);
+        for i := 1 to rowLength do // parse digits
         begin
-            heatmap[rowCount][i - 1] := ord(line[i]) - ord('0');
+            hmap[rowCount, i - 1] := ord(line[i]) - ord('0');
         end;
         Inc(rowCount);
     end;
     Close(inputFile);
 
-    riskLevelSum := 0;
-    for i := low(heatmap) to high(heatmap) do
-    begin
-        for j := low(heatmap[i]) to high(heatmap[i]) do
-        begin
-            isLowPoint := true;
-            for m := -1 to 1 do
-            begin
-                for n := -1 to 1 do
-                begin
-                    if not ((m = 0) and (n = 0)) and
-                       (low(heatmap) <= i + m) and (i + m <= high(heatmap)) and
-                       (low(heatmap[i]) <= j + n) and (j + n <= high(heatmap[i])) then
-                        isLowPoint := isLowPoint and (heatmap[i][j] < heatmap[i + m][j + n]);
-                end;
-            end;
+    FindLowPoints(hmap);
 
-            if isLowPoint then
-                riskLevelSum += 1 + heatmap[i][j];
-        end;
-    end;
+    part1 := solvePart1();
+    Writeln('Part 1: ', part1);
 
-    Writeln('Part 1: ', riskLevelSum);
-    // Writeln('Part 2: ');
+    part2 := solvePart2();
+    Writeln('Part 2: ', part2);
 end
 else Writeln('ERROR: An input file is required!');
 
